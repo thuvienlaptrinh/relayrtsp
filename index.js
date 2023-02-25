d0caa9f39465020cfaff2b896a7dfd0851a4b91d
thuvienlaptrinh <dangtuanphong@hotmail.com>
2023-02-26 06:56:39 +0700

    change

--- index.js
+++ index.js
@@ -63,36 +63,15 @@ app.ws("/api/stream/:cameraID", (ws, req) => {
   }
 });
 
-app.ws("/rtsp", handler)
+app.ws("/rtsp", handler);
 
 // this is an example html page to view the stream
-app.get("/", (req, res) =>
-  res.send(`
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang xem rtsp</title>
-	<script src='${scriptUrl}'></script>
-</head>
-<body>
-    <canvas id='canvas'></canvas>
-	<script>
-    loadPlayer({
-      url: 'ws://' + location.host + '/rtsp',
-      canvas: document.getElementById('canvas')
-    });
-  </script>
-</body>
-</html>
-`)
-);
+app.get("/", (req, res) => res.sendFile(__dirname + "/homepage.html"));
 
 app
   .listen(7000, function () {
     console.log(`${currentTime}: Node map App listening at port 7000`);
+    console.log(`${currentTime}: ${scriptUrl}`);
   })
   .on("error", function (err) {
     console.log(`${err}: Node map error`);
