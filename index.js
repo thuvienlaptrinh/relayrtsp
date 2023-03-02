7a093dfb9ff431b103240ba2c2c479e0dc2d3fdb
thuvienlaptrinh <dangtuanphong@hotmail.com>
2023-03-02 22:13:01 +0700

    update fps

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ const CameraData = JSON.parse(
 var cams = CameraData.map(function (T, i) {
   var stream = new rtsp.FFMpeg({
     input: `rtsp://${T.username}:${T.pwd}@${T.url}`,
-    resolution: "640x360",
+    resolution: "1920x1080",
     quality: 4,
     camid: T.id,
+    rate: 25
   });
   stream.camid = T.id;
   stream.on("start", function () {
