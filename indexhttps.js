dfbd553ecc6f2e99115ce3d2c8df89c31a11760d
thuvienlaptrinh <dangtuanphong@hotmail.com>
2023-03-03 14:20:57 +0700

    change port

--- indexhttps.js
+++ indexhttps.js
@@ -13,8 +13,8 @@ const rtsp = require("rtsp-ffmpeg");
 
 
 app.use(express.static("public"));
-server.listen(7043, function () {
-  console.log("Listening https on port:7043");
+server.listen(7001, function () {
+  console.log("Listening https on port:7001");
 });
 
 const CameraData = JSON.parse(
