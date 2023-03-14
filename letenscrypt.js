7c58b061bef664d82e8a1d64501b335b46483c90
thuvienlaptrinh <dangtuanphong@hotmail.com>
2023-03-14 18:45:08 +0700

    update

--- letenscrypt.js
+++ letenscrypt.js
@@ -8,3 +8,7 @@ const HTTP_PORT = 80;
 const HTTP_SERVER = http.createServer(app).listen(HTTP_PORT, function () {
   console.log(`Listening on localhost:${HTTP_PORT}`);
 });
+
+app.get("/*", function (req, res) {
+  res.send("Xin Chao tu server");
+});
